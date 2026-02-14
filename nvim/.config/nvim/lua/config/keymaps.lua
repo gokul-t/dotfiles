24d21073637b7d8902dcd692ee055fb077a9be3c
Gokul T <gokultmail@gmail.com>
2026-02-14 21:49:47 +0530

    nvim keymap utils removed

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = require("utils.keymaps").map
-local xnoremap = require("utils.keymaps").xnoremap
--- local nnoremap = require("utils.keymaps").nnoremap
 
-map("t", "<C-[>", "<C-\\><C-n>", { noremap = true, desc = "Exit terminal mode" })
-
-xnoremap("<leader>p", [["_dP]])
+vim.keymap.set("t", "<C-[>", "<C-\\><C-n>", { noremap = true, desc = "Exit terminal mode" })
+vim.keymap.set(
+  "x",
+  "<leader>p",
+  [["_dP]],
+  { noremap = true, desc = "delete and paste without overwriting the default register" }
+)
