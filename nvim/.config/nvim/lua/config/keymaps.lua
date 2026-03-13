0ee770e495042b0240bb150461d8efcef0308759
Gokul T <gokultmail@gmail.com>
2026-03-13 18:31:07 +0530

    JAVA_HOME_PATH added

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = vim.keymap.set
+local map = require("utils.keymaps").map
+local xnoremap = require("utils.keymaps").xnoremap
+-- local nnoremap = require("utils.keymaps").nnoremap
 
 map("t", "<C-[>", "<C-\\><C-n>", { noremap = true, desc = "Exit terminal mode" })
+
+xnoremap("<leader>p", [["_dP]])
